059899e14bca037da642883653cf26cf8a43f799
PierreBouchet <pierrebouchet39@gmail.com>
2023-12-15 09:07:01 +0100

    First commit

---
+++ src/main/java/com/gylderia/polis/Polis.java
@@ -0,0 +1,17 @@
+package com.gylderia.polis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Polis extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
