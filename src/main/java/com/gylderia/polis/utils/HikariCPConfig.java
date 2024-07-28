0b0bc3e83866e45341a9ec6c39d433490ec50023
PierreBouchet <pierrebouchet39@gmail.com>
2024-07-28 20:09:30 +0200

    fix : création du dossier mysql/config.yml  s'il n'existe pas.

--- src/main/java/com/gylderia/polis/utils/HikariCPConfig.java
+++ src/main/java/com/gylderia/polis/utils/HikariCPConfig.java
@@ -7,12 +7,25 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 public class HikariCPConfig {
     private HikariDataSource dataSource;
 
     public HikariCPConfig(Polis plugin) {
         File configFile = new File(plugin.getDataFolder(), "mysql/config.yml");
+        if (!configFile.exists()) {
+            try {
+                File configDir = new File(plugin.getDataFolder(), "mysql");
+                if (!configDir.exists()) {
+                    configDir.mkdirs();
+                }
+                configFile.createNewFile();
+                // Optionally, you can write default configuration values to the file here
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl("jdbc:mysql://" + config.getString("mysql.host") + ":" + config.getInt("mysql.port") + "/" + config.getString("mysql.database"));
